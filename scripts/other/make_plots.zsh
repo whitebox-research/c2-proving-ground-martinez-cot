49ef8da36d88586bb2504cd31e14c5463096b268
sepiatone <aditya.thomas@alum.mit.edu>
2025-06-10 12:24:25 +0530

    remove files not required

--- scripts/other/make_plots.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-for file in d/questions/**/*.yaml; do
-    # :t gets the tail (filename), :r removes extension
-    dataset_id=${file:t:r}
-    if [[ $dataset_id = *tests* ]]; then
-        continue
-    fi
-    ./scripts/plot_direct_vs_cot_acc.py -d "$dataset_id" -i "instr-v0"
-done
