542e311d15ccb21f06484b0692f4b035fa4a14ee
sepiatone <aditya.thomas@alum.mit.edu>
2025-06-10 15:24:13 +0530

    clean up of src and data folders

--- chainscope/__init__.py
+++ chainscope/__init__.py
@@ -3,7 +3,7 @@ from importlib.resources import files
 from beartype.claw import beartype_this_package
 
 beartype_this_package()
-_data_dir = files("chainscope.data")
+_data_dir = files("data")
 DATA_DIR = _data_dir._paths[0]  # type: ignore
 
 __version__ = "0.1.0"
